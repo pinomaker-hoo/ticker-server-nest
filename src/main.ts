974fd62e7d9ae9d7d7083df0514dade7478dc7f6
pino._.hoo <inhoo23@naver.com>
2022-10-01 09:57:43 +0900

    add : cors + 환경 변수

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+  await app.listen(process.env.NODE_SERVER_PORT);
 }
 bootstrap();
