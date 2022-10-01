f1155f6d20f4291cfe30742a03d4d039dc30944c
pino._.hoo <inhoo23@naver.com>
2022-10-01 15:15:53 +0900

    fix : update module

--- src/auth/passport/auth.kakao.strateyg.ts
+++ src/auth/passport/auth.kakao.strateyg.ts
@@ -1,25 +1,25 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-kakao';
+import { PassportStrategy } from "@nestjs/passport"
+import { Strategy } from "passport-kakao"
 
 export class KakaoStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      clientID: '35925e2699a55c7bffbb6e051da01a40',
-      callbackURL: 'http://localhost:3001/auth/kakao/callback',
-    });
+      clientID: "38378557cacb552e242b361bd5511cdd",
+      callbackURL: "http://localhost:3001/auth/kakao/callback",
+    })
   }
 
   async validate(accessToken, refreshToken, profile, done) {
-    const profileJson = profile._json;
-    const kakao_account = profileJson.kakao_account;
+    const profileJson = profile._json
+    const kakao_account = profileJson.kakao_account
     const payload = {
       name: kakao_account.profile.nickname,
       kakaoId: profileJson.id,
       email:
         kakao_account.has_email && !kakao_account.email_needs_agreement
           ? kakao_account.email
-          : 'null',
-    };
-    done(null, payload);
+          : "null",
+    }
+    done(null, payload)
   }
 }
